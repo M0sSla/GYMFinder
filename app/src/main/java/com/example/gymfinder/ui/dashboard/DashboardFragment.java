802ddecf80b583bac00e79a97b353ded1d925339
MosSla <mossla@inbox.ru>
2023-04-02 18:20:00 +0300

    updated bottom nav. view and added new fragment - fragment_find

--- app/src/main/java/com/example/gymfinder/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/gymfinder/ui/dashboard/DashboardFragment.java
@@ -10,18 +10,18 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.gymfinder.databinding.FragmentDashboardBinding;
+import com.example.gymfinder.databinding.FragmentTrainingBinding;
 
 public class DashboardFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentTrainingBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         DashboardViewModel dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentTrainingBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textDashboard;
