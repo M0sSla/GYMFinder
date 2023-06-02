e097dacd037fe1735b98b37ac29732130cf2def4
MosSla <mossla@inbox.ru>
2023-06-02 23:13:17 +0300

    finder fragment updated. Added animation for swipe.

--- app/src/main/java/com/example/gymfinder/ui/finder/FinderFragment.java
+++ app/src/main/java/com/example/gymfinder/ui/finder/FinderFragment.java
@@ -1,9 +1,12 @@
 package com.example.gymfinder.ui.finder;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.gymfinder.R;
 import com.example.gymfinder.databinding.FragmentFinderBinding;
 
 public class FinderFragment extends Fragment {
@@ -26,6 +30,38 @@ public class FinderFragment extends Fragment {
         View root = binding.getRoot();
         final TextView textView = binding.textFinder;
         finderViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        @SuppressLint("ResourceType") Animation swipeLeftAnimation = AnimationUtils.loadAnimation(getContext(), R.drawable.swipe_left);
+        @SuppressLint("ResourceType") Animation swipeRightAnimation = AnimationUtils.loadAnimation(getContext(), R.drawable.swipe_right);
+
+        binding.dislike2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.Fragment.startAnimation(swipeLeftAnimation);
+            }
+        });
+
+        binding.like2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.Fragment.startAnimation(swipeRightAnimation);
+            }
+        });
+
+        binding.dislike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.Fragment.startAnimation(swipeLeftAnimation);
+            }
+        });
+
+        binding.like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.Fragment.startAnimation(swipeRightAnimation);
+            }
+        });
+
         return root;
     }
 
